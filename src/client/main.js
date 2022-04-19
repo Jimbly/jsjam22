6caf563bc4191f42cb511deba34af5ba08e69882
Jimb Esser <wasteland@gmail.com>
2022-04-19 08:47:39 -0700

    Allow placing some tiles

--- src/client/main.js
+++ src/client/main.js
@@ -4,13 +4,15 @@ local_storage.setStoragePrefix('jsjam22'); // Before requiring anything else tha
 
 const camera2d = require('glov/client/camera2d.js');
 const engine = require('glov/client/engine.js');
+const input = require('glov/client/input.js');
+const { floor } = Math;
 const net = require('glov/client/net.js');
 const pico8 = require('glov/client/pico8.js');
 const { mashString, randCreate } = require('glov/common/rand_alea.js');
 const { createSprite } = require('glov/client/sprites.js');
 const { createSpriteAnimation } = require('glov/client/sprite_animation.js');
 const ui = require('glov/client/ui.js');
-const { vec2 } = require('glov/common/vmath.js');
+const { vec2, vec4 } = require('glov/common/vmath.js');
 
 window.Z = window.Z || {};
 Z.BACKGROUND = 1;
@@ -25,13 +27,16 @@ let sprites = {};
 
 const TILE_SIZE = 16;
 
-const TILE_EMPTY = 0;
-const TILE_ROAD = 1;
-// const TILE_SOURCE = 2;
-const TILE_DETAIL = 3;
+const TYPE_EMPTY = 0;
+const TYPE_DETAIL = 1;
+const TYPE_SOURCE = 2;
+const TYPE_SINK = 3;
+const TYPE_ROAD = 4;
 
 let rand;
 let game_state;
+
+const color_ghost = vec4(1, 1, 1, 0.5);
 
 function gameStateCreate() {
   rand = randCreate(mashString('test'));
@@ -42,7 +47,7 @@ function gameStateCreate() {
     let row = [];
     for (let xx = 0; xx < w; ++xx) {
       row.push({
-        tile: TILE_EMPTY,
+        type: TYPE_EMPTY,
       });
     }
     board.push(row);
@@ -51,7 +56,7 @@ function gameStateCreate() {
     let x = rand.range(w);
     let y = rand.range(h);
     let cell = board[y][x];
-    cell.tile = TILE_DETAIL;
+    cell.type = TYPE_DETAIL;
     cell.anim = createSpriteAnimation({
       idle: {
         frames: [2,3],
@@ -64,10 +69,10 @@ function gameStateCreate() {
   }
   // 4x6 road at 2,4
   for (let ii = 0; ii < 4; ++ii) {
-    board[4][2+ii].tile = TILE_ROAD;
-    board[9][2+ii].tile = TILE_ROAD;
-    board[5+ii][2].tile = TILE_ROAD;
-    board[5+ii][5].tile = TILE_ROAD;
+    board[4][2+ii].type = TYPE_ROAD;
+    board[9][2+ii].type = TYPE_ROAD;
+    board[5+ii][2].type = TYPE_ROAD;
+    board[5+ii][5].type = TYPE_ROAD;
   }
   return {
     w, h,
@@ -86,8 +91,59 @@ function init() {
   game_state = gameStateCreate();
 }
 
+const SHOP = [
+  {
+    name: 'Tree',
+    cell: {
+      type: TYPE_SOURCE,
+      frame: 1,
+    },
+  },
+  {
+    name: 'Output',
+    cell: {
+      type: TYPE_SINK,
+      frame: 4,
+    },
+  },
+
+  {
+    name: 'Debug',
+    cell: {
+      type: TYPE_ROAD,
+      frame: 0,
+    },
+    debug: true,
+  },
+];
 function drawShop(x0, y0, w, h) {
-  ui.drawRect2({ x: x0, y: y0, w, h, color: pico8.colors[15], z: Z.UI - 1 });
+  const PAD = 4;
+  const BUTTON_H = 26;
+  const BUTTON_W = 48;
+  let x = x0;
+  let y = y0;
+  ui.drawRect2({ x, y, w, h, color: pico8.colors[15], z: Z.UI - 1 });
+  x += PAD;
+  y += PAD;
+  w -= PAD*2;
+  h -= PAD*2;
+  for (let ii = 0; ii < SHOP.length; ++ii) {
+    let elem = SHOP[ii];
+    if (elem.debug && !engine.DEBUG) {
+      continue;
+    }
+    if (ui.button({
+      x, y, img: sprites.tiles, frame: elem.cell.frame,
+      h: BUTTON_H,
+      w: BUTTON_W,
+    })) {
+      if (game_state.cursor) {
+        // return funds
+      }
+      game_state.cursor = elem;
+    }
+    y += BUTTON_H + PAD;
+  }
 }
 
 function drawBoard(x0, y0, w, h) {
@@ -106,11 +162,11 @@ function drawBoard(x0, y0, w, h) {
       let x = xx * TILE_SIZE;
       let y = yy * TILE_SIZE;
       let frame = null;
-      switch (cell.tile) { // eslint-disable-line default-case
-        case TILE_ROAD:
+      switch (cell.type) { // eslint-disable-line default-case
+        case TYPE_ROAD:
           frame = 0;
           break;
-        case TILE_DETAIL:
+        case TYPE_DETAIL:
           frame = cell.anim.getFrame(dt);
           break;
       }
@@ -119,9 +175,34 @@ function drawBoard(x0, y0, w, h) {
           x, y, z: Z.BOARD, frame,
         });
       }
+      if (input.click({
+        x, y, w: TILE_SIZE, h: TILE_SIZE,
+      })) {
+        if (game_state.cursor) {
+          cell.type = game_state.cursor.cell.type;
+          if (!input.keyDown(input.KEYS.SHIFT)) {
+            game_state.cursor = null;
+          }
+        }
+      }
     }
   }
+  let mouse_over = input.mouseOver({ x: 0, y: 0, w, h });
+  if (game_state.cursor) {
+    if (mouse_over) {
+      let mouse_pos = input.mousePos();
 
+      let x = floor(mouse_pos[0] / TILE_SIZE);
+      let y = floor(mouse_pos[1] / TILE_SIZE);
+      sprites.tiles.draw({
+        x: x * TILE_SIZE,
+        y: y * TILE_SIZE,
+        z: Z.UI,
+        color: color_ghost,
+        frame: game_state.cursor.cell.frame,
+      });
+    }
+  }
 
   camera2d.pop();
 }
@@ -142,7 +223,7 @@ export function main() {
   const font_info_04b03x2 = require('./img/font/04b03_8x2.json');
   const font_info_04b03x1 = require('./img/font/04b03_8x1.json');
   const font_info_palanquin32 = require('./img/font/palanquin32.json');
-  let pixely = 'strict';
+  let pixely = 'on';
   let font;
   if (pixely === 'strict') {
     font = { info: font_info_04b03x1, texture: 'font/04b03_8x1' };
@@ -165,8 +246,8 @@ export function main() {
   }
   font = engine.font;
 
-  ui.scaleSizes(13 / 32);
-  ui.setFontHeight(8);
+  ui.scaleSizes(26 / 32);
+  ui.setFontHeight(16);
 
   init();
 
